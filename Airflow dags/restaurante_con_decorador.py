5017d07a4365bff2263641aa22f3fa442a4a0d50
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2021-10-19 19:08:54 -0500

    LAMBDA - MAP - FILTER -  REDUCE

--- Airflow dags/restaurante_con_decorador.py
+++ Airflow dags/restaurante_con_decorador.py
@@ -1,9 +1,15 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
-
-@dag(schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False, tags=['Almuerzo'])
-def ejemplo_rutina_restaurante_con_decorador():
+# https://airflow.apache.org/docs/apache-airflow/stable/concepts/index.html
+# https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html
+# https://airflow.apache.org/docs/apache-airflow/stable/configurations-ref.html#parsing-processes
+# https://airflow.apache.org/docs/apache-airflow/stable/configurations-ref.html#worker-concurrency
+# https://airflow.apache.org/docs/apache-airflow/stable/configurations-ref.html#max-active-runs-per-dag
+# https://airflow.apache.org/docs/apache-airflow/stable/configurations-ref.html#max-active-tasks-per-dag
+# https://airflow.apache.org/docs/apache-airflow/stable/configurations-ref.html#parallelism
+@dag(schedule_interval='0 5 * * *', concurrency=10,  max_active_runs=3, start_date=datetime(2021, 1, 1), catchup=False, tags=['Almuerzo'])
+def ejemplo_rutina_restaurante_con_decorador_2():
 
     @task()
     def conocer_menu():
@@ -25,4 +31,4 @@ def ejemplo_rutina_restaurante_con_decorador():
     
     conocer_menu() >> realizar_pedido() >> disfrutar_los_alimentos()
 
-dag = ejemplo_rutina_restaurante_con_decorador()
+dag = ejemplo_rutina_restaurante_con_decorador_2()
