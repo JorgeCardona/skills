0d26c42faa0b4f90dbf4cf4ad79f2f28e445d305
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2021-11-21 16:14:31 -0500

    adicionadas pruenas unitarias pytest

---
+++ Python/Notebooks/datos.py
@@ -0,0 +1,17 @@
+import time
+
+def esperar_5_segundos():    
+    time.sleep(5)
+    return 5
+
+def esperar_3_segundos():    
+    time.sleep(3)
+    return 3j
+
+
+def funcion_dada():
+    
+    s = esperar_5_segundos()
+    s = esperar_3_segundos()
+    
+    return s
