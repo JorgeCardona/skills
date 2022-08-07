4b443636cd852c5a985d42086b003a14cf9ff816
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2022-08-07 00:55:47 -0500

    docker file cluster hadoop 2.10.2, java 8 y spark 2.48-hadoop-2.7

---
+++ Hadoop-Spark Dockerizados/5 Datanodes - 1 Namenode - Hadoop v2.10.2 -  Spark v2.4.8-bin-hadoop2.7 - Java v Openjdk-8-jdk/docker/master/config/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+service ssh start
+
+# start cluster
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh 
+
+# create paths and give permissions
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/root/input_answers
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/root/input_names
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal /data/user_ids_answers input_answers
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal /data/user_ids_names input_names
+$HADOOP_HOME/bin/hdfs dfs -mkdir /spark-logs
+
+# start spark history server
+$SPARK_HOME/sbin/start-history-server.sh
+
+# run the spark job
+spark-submit --deploy-mode cluster --master yarn \
+               --class StackAnswer \
+               $SPARK_HOME/jars/stackanswer_2.12-1.0.jar
+
+# copy results from hdfs to local
+$HADOOP_HOME/bin/hdfs dfs -copyToLocal /user/root/users_most_actives /data
+$HADOOP_HOME/bin/hdfs dfs -copyToLocal /user/root/locations_most_actives /data
+
+bash
