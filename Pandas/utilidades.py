e2baa29d924d3a92e80059688c808762e6698064
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2022-10-15 23:09:27 -0500

    adicionados archivos, json, parquet, csv, excel, html para leer con Pandas

---
+++ Pandas/utilidades.py
@@ -0,0 +1,84 @@
+animales = {
+   
+    'Mamíferos':['Ballenas','Capibaras','Ratones','Delfines','Ovejas','Canguros','Orcas','Elefantes','Chimpancés','Perros','Jirafas','Gorilas','Gatos','Rinocerontes','Seres humanos','Caballos','Leones','Cabras','Vacas','Hienas'],
+    'Reptiles':['Víboras','Basiliscos','Lagartos voladores australianos','Cocodrilos','Boas constrictoras','Monstruos de Gila','Aligátores','Tortugas galápagos','Luciones','Iguanas','Tortugas terrestres','Dinosaurios (extintos)','Tortugas acuáticas','Varanos','Pterosaurios (extintos)','Lagartijas comunes','Camaleones','Mosasaurios (extintos)','Dragones de Komodo','Tuátaras de Nueva Zelanda'],
+    'Peces':['Tiburones','Bacalaos','Peces vela','Anguilas de mar','Peces sapo','Atunes','Barracudas','Koi japoneses','Peces espada','Peces globo','Carpas','Peces abisales','Peces payaso','Esturiones','Celacantos (extintos)','Las agujas de río','Pirañas','Lenguados','Los tiburones ballena','Peces luna'],
+    'Anfibios':['Ajolotes','Sapos corredores','Ranas toro africanas','Ranas arborícolas','Sapos gigantes','Salamandras gigantes chinas','Cecilias','Ranas venenosas','Necturos','Salamandras comunes','Gallipatos','Microsaurios (extintos)','Ranas arcoíris','Salamandras sin pulmón','Lisorofios (extintos)','Ranas de Darwin','Ranas de Seychelles','Proteos','Sapos comunes','Sapos de caña venenosos'],
+    'Aves':['Gallinas','Pájaros carpinteros','Guacamayas','Patos','Lechuzas','Loros','Buitres','Cóndores','Águilas','Cuervos','Colibríes','Avestruces','Tucanes','Canarios','Cigüeñas','Pavos','Gaviotas','Pavorreales','Faisanes','Pelícanos'],
+    
+    'Vertebrados':['Ballenas','Capibaras','Ratones','Delfines','Ovejas','Canguros','Orcas','Elefantes','Chimpancés','Perros','Jirafas','Gorilas','Gatos','Rinocerontes','Seres humanos','Caballos','Leones','Cabras','Vacas','Hienas','Víboras','Basiliscos','Lagartos voladores australianos','Cocodrilos','Boas constrictoras','Monstruos de Gila','Aligátores','Tortugas galápagos','Luciones','Iguanas','Tortugas terrestres','Dinosaurios (extintos)','Tortugas acuáticas','Varanos','Pterosaurios (extintos)','Lagartijas comunes','Camaleones','Mosasaurios (extintos)','Dragones de Komodo','Tuátaras de Nueva Zelanda','Tiburones','Bacalaos','Peces vela','Anguilas de mar','Peces sapo','Atunes','Barracudas','Koi japoneses','Peces espada','Peces globo','Carpas','Peces abisales','Peces payaso','Esturiones','Celacantos (extintos)','Las agujas de río','Pirañas','Lenguados','Los tiburones ballena','Peces luna','Ajolotes','Sapos corredores','Ranas toro africanas','Ranas arborícolas','Sapos gigantes','Salamandras gigantes chinas','Cecilias','Ranas venenosas','Necturos','Salamandras comunes','Gallipatos','Microsaurios (extintos)','Ranas arcoíris','Salamandras sin pulmón','Lisorofios (extintos)','Ranas de Darwin','Ranas de Seychelles','Proteos','Sapos comunes','Sapos de caña venenosos','Gallinas','Pájaros carpinteros','Guacamayas','Patos','Lechuzas','Loros','Buitres','Cóndores','Águilas','Cuervos','Colibríes','Avestruces','Tucanes','Canarios','Cigüeñas','Pavos','Gaviotas','Pavorreales','Faisanes','Pelícanos']    ,
+    'Invertebrados':['Moscas domésticas','Tábano bombilius','Arañas','Avispas excavadoras','Mariposa limonera','Mariposa saltacercas','Orugas','Libélula emperador','Libélulas','Escorpiones','Mariposa sesia','Escarabajo','Mariposa macaón','Mariposa blanquita de la col','Araña de patas largas','Hormigas león','Polilla','Luciérnaga','Avispas alfareras','Escarabajo fitófago','Araña cangrejo','Chinche roja, zapatero','Curilla o aceitera','Cigarra o chicharra','Mariquitas','Caballito del diablo','Avispa scolia','Mariposa cervantes','Termes','Ácaros','Abeja de la miel','Mosquitos','Moscardones','Caracoles','Cochinillas','Tijeretas','Garrapata','Mariposa dorada','Piojos','Cucarachas','Mariposa pavo real','Mariposa del almez','Pulgas','Abejorros','Mantis religiosa','Medusas','Grillos','Moscas cernícalo','Mariposa esfinge colibrí','Avispas portasierra','Mariposa nacarada','Estrellas de mar','Hormigas','Chinches','Tábanos','Escarabajos acuáticos','Milpiés','Larvas','Cochinillas de humedad','Pulgones','Escarabajos','Mariposa medioluto','Avispas sociales','Mariposa ajedrezada menor','Mariposa ícaro','Abejas','Saltamontes','Mariposa lobito agreste','Ciempiés'],
+    'Ovíparos':['Cocodrilo','Hormiga','Pingüino','Gorrión','Gallina','Loro','Cóndor','Águila','Avestruz','Cigüeña','Pato','Ganso','Víbora de cascabel','Codorniz','Rana','Guacamayo','Hornero','Salmón','Abeja','Paloma'],
+    'Frugívoros':['Ardilla','Mono aullador','Loro','Bonobo','Mosca de la fruta','Macaco','Calandria','Murciélago frugívoro','Iguana','Chimpancé','Tupaya enana','Conejo del campo','Chinche del campo','Perico','Tucán','Gibón','Pacú','Zarigüeya','Gorila','Tortuga terrestre','Zorro volador','Lémur','Tapir','Orangután','Lirón','Mono Tití'],   
+    'Rumiantes':['Vaca','Toro','Oveja','Alce','Cabra','Llama','Cebú','Búfalo cafre','Ciervo común','Nilgó','Antílope de cuatro cuernos','Gamo común','Sitatunga','Niala','Ciervo de copete','Anoa de montaña','Bongo','Alce irlandés','Ñu','Venado temazate','Sambar','Gayal','Bisonte estepario','Ciervo porcino','Ciervo de los pantanos','Búfalo de agua','Gacela de Grant','Corzo','Antílope jeroglífico','Candelillo','Venado andino','Bisonte americano','Corzuela','Vicuña','Bisonte europeo','Oryx','Urial','Venado de las Pampas','Uapití','Anoa de llanura','Reno','Apaca','Íbice de los Alpes','Ciervo axis','Impala'],    
+    
+    'Herbívoros':['Cebra','Oveja','Antílope','Llama','Cabra','Alpaca','Jirafa','Koala','Elefante','Ciervo','Tapir','Ardilla','Vaca','Canguro','Caballo','Cobayo','Rinoceronte','Burro','Conejo','Tortuga'],
+    'Carnívoros':['Foca','Hiena','Lince','Gato','Jaguar','Lobo','Gato montés','León','Lobo gris','Comadreja','León marino','Civeta','Coyote','Leopardo','Mangosta','Marta','Cachalote','Tigre siberiano','Ballena azul','Delfín','Tigre de bengala','Ballena jorobada','Oso pardo','Orca','Beluga','Oso polar','Nutria','Narval','Guepardo','Gineta manchada','Perro','Puma','Panda rojo','Pantera negra','Gineta común','Linsangs','Fosa','Murciélago espectral','Mapache','Visón europeo','Murciélago pescador','Demonio de Tasmania','Serval','Morsa','Chacal','Pangolín','Hurón','Glotón','Tejón','Garduña','Kinkajú','Anaconda','Cobra','Tortuga marina','Boa','Pitón','Varano del desierto','Cocodrilo','Tortuga lagarto','Caimán','Dragón de Komodo','Gecko leopardo','Serpiente de coral','Atún','Pez espada','Muskallonga americano','Tiburón blanco','Perca','Marlín','Tiburón martillo','Salmón','Pez gato','Tiburón tigre','Tollo cigarro','Piraña','Tiburón peregrino','Tiburón toro','Barracuda','Aguila arpía','Albatros','Buitre leonado','Águila pescadora','Gaviota','','Buitre quebrantahuesos','Secretario','Halcón','Buitre común','Pingüino','Cuervo','Buitre negro','Pelícano','Cóndor de California','Marabú','Milano','Cóndor andino','Lechuza','Alimoche','Búho','Gavilán pollero'],
+    'Omnívoros':['Humano','Seres humanos','Erizo','Mirlo','Perdiz','Flamingo','Bacalao','Gaviota','Garcilla bueyera','Focha','Casuario','Zorzal','Pájaro carpintero','Zorrillo','Perro','Delfín','Grajo','Pez globo','Pinzón','Labeo bicolor','Hormiga','Garza real','Petirrojo','Morena','Jabalí','Cerdo','Tucán','Gorrión','Mono','Urraca','Gallina','Corydora','Zarigüeya','Cacatúa','Cangrejo','Avispa','Pez espiga','Tiburón','Rinoceronte','Langosta','Ballena','Avestruz','Tortugas','Faisán','Cisne','Gatos','Mosca','Bengalí rojo','Oso','Hámster','Cuervos','Ñandú','Bagre','Avutarda','Lémur','Zorro','Armadillo','Pez arquero','Mofeta','Mapache','Gorila','Chimpancé','Ardilla','Ornitorrinco','Emú','Grillo','Avestruces','Ratón','Carpa','Cucaracha','Escarabajo de cementerio','Pavo real','Ganso','Coyote','Piraña','Coatí','Manatí','Grulla','Mojarrita','Ratones','Nutria','Jerbo','Casuarios','Tejón','Tortuga','Tortuga carbonaria','Espátula','Perezoso','Ayeaye','Calamón'],
+}
+
+
+def crear_dataframes(limite_muestra = 100):
+    import random
+    import itertools
+    import pandas as pd
+    
+    # obtiene la lista de llaves del diccionario
+    lista_llaves = list(animales.keys())
+    
+    # elige las llaves para cada diccionario, la primera lista contiene los 5 primeros valores, la segunda los 5 al 10
+    # la tercera lista contiene una fila aleatoria de  la primera lista, una de la segunda y los ultimos 3 valores del diccionario
+    llaves_seleccionadas =  [[value for value in range(0,5)], [value for value in range(5,10)], [random.randint(0,5)] + [random.randint(5,10)] + [10,11,12]]
+    
+    # guarda el diccionario final, que tiene 3 elementos entonces se ponen indices del 0 al 2
+    lista_diccionarios_generados = [0,1,2]
+    
+    # recorre la lista de listas creada
+    for indice, valores in enumerate(llaves_seleccionadas):
+    
+        # crea una lista con los nombres de cada columna que se va a usar en el diccionario
+        lista_filtrada = [lista_llaves[valor] for valor in llaves_seleccionadas[indice]]
+        
+        # crea un diccionario usando las tuplas como base de la comprehension list
+        # trae todos los valores de cada llave y la guarda en la tupla que se convierte en diccionario
+        lista_diccionarios = dict([(llave,valor) for llave, valor in animales.items() if llave in lista_filtrada])
+        
+        # lista para almacenar los valores temporalmente
+        lista_diccionarios_interno = []
+
+        # recorrer la lista de diccionarios y eleige valores al azar para contruir el nuevo diccionario
+        for llave, valor in lista_diccionarios.items():
+            
+            # crea una lista de ID que sera la primera columna de cada dataframe
+            identificadores = {
+                'Id': [valor_id for valor_id in range(1, limite_muestra + 1)],
+                'Lider' : [random.randint(1,limite_muestra + 1) for limite in range(limite_muestra)]
+            }
+            
+            identificador = ([
+                        ('Id',[valor_id for valor_id in range(1, limite_muestra + 1)])
+                    ])
+            lider = ([
+                        ('Lider',[random.randint(1,limite_muestra + 1) for limite in range(limite_muestra)])
+                    ])
+            
+            diccionario = ([
+                            (llave,[random.choice(valor) for i in range(limite_muestra)])
+                          ])
+            
+            # guarda la valores como lista de tuplas para convertir a diccionario facilmente
+            lista_diccionarios_interno.append(tuple(itertools.chain(*identificador)))
+            lista_diccionarios_interno.append(tuple(itertools.chain(*lider)))
+            lista_diccionarios_interno.append(tuple(itertools.chain(*diccionario)))
+            
+        lista_diccionarios_generados[indice] = lista_diccionarios_interno
+
+    # captura la informacion de la primera parte de la lista como tupla y la convierte a diccionario casteandola con dict
+    diccionario_0 = dict(lista_diccionarios_generados[0])
+   
+    diccionario_1 = dict(lista_diccionarios_generados[1])
+    
+    diccionario_2 = dict(lista_diccionarios_generados[2])
+    
+    # retorna los diccionarios como dataframes usando la funcion creada para esa conversion
+    return pd.DataFrame(diccionario_0), pd.DataFrame(diccionario_1), pd.DataFrame(diccionario_2)
