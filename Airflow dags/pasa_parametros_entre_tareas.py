3d723446063fa936a5084ae1501cb4193e53bf62
JorgeCardona <jorgecardona@utp.edu.co>
2023-02-27 19:47:19 -0500

    adidionado eliminacion de contenido de las variables en xcom

--- Airflow dags/pasa_parametros_entre_tareas.py
+++ Airflow dags/pasa_parametros_entre_tareas.py
@@ -1,11 +1,4 @@
-from airflow.decorators import dag, task
-from datetime import datetime
-
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
 import random
-
 
 def get_number():
     
@@ -18,10 +11,259 @@ def get_duplicate_number(number):
 def get_len(number):
     return len(str(number))
     
+
+########################################################################################################################################################################
+############################################################# TASK DECORATOR FUNCION COMO PARAMETRO ####################################################################
+########################################################################################################################################################################
+
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+
+
+url = "http://catfact.ninja/fact"
+
+default_args = {"start_date": datetime(2021, 1, 1)}
+
 @dag(
-    dag_id="pasa_resultado_de_funcion_como_parametro",
+    dag_id="funcion_como_parametro",
     description='descripcion del dag creado',
-    tags=['FUNCION_COMO_PARAMETRO'],
+    tags=['PASA_VALORES_ENTRE_TAREAS'],
+    schedule_interval='@monthly', # https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html#dag-runs
+    concurrency=10,  
+    max_active_runs=3, 
+    start_date=datetime(2000, 1, 1), 
+    end_date=datetime(2029, 12, 11), 
+    catchup=False,
+    default_args={
+                "dag_owner": "https://hithub.com/jorgecardona",
+                "owner": "Jorge Cardona",  # This defines the value of the "owner" column in the DAG view of the Airflow UI
+                "retries": 2,  # If a task fails, it will retry 2 times.
+                "retry_delay": timedelta(minutes=3, ), # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+                "do_xcom_push":False,
+                "do_xcom_pull":False
+                }
+    )
+def taskflow():
+    @task
+    def tarea_numero():
+        return get_number()
+
+    @task
+    def tarea_duplicar(number):
+        
+        if number %2 ==0:
+            return 13
+        
+        return get_duplicate_number(number)
+
+    @task
+    def tarea_size(number):
+        
+        return get_len(number)
+            
+    # Invoke functions to create tasks and define dependencies
+    tarea_size(tarea_duplicar(tarea_numero()))
+
+taskflow()
+
+
+########################################################################################################################################################################
+############################################################## TASK DECORATOR XCOM_PUSH - XCOM_PULL ####################################################################
+########################################################################################################################################################################
+
+    
+@dag(
+    dag_id="resultado_como_parametro",
+    description='descripcion del dag creado',
+    tags=['PASA_VALORES_ENTRE_TAREAS'],
+    schedule_interval='@monthly', # https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html#dag-runs
+    concurrency=10,  
+    max_active_runs=3, 
+    start_date=datetime(2000, 1, 1), 
+    end_date=datetime(2029, 12, 11), 
+    catchup=False,
+    default_args={
+                "dag_owner": "https://hithub.com/jorgecardona",
+                "owner": "Jorge Cardona",  # This defines the value of the "owner" column in the DAG view of the Airflow UI
+                "retries": 2,  # If a task fails, it will retry 2 times.
+                "retry_delay": timedelta(minutes=3, ), # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+                "do_xcom_push":False,
+                "do_xcom_pull":False
+                }
+    )
+def ejemplo_rutina_llama_codigo():
+    
+    valor_aleatorio = random.randint(1, 20_000)
+    
+    @task(task_id='tarea_extraer_usando_parametro_del_decorador')
+    def tarea_numero(ti=None):
+        # guarda el valor de retorno de VALUE=mostrar(valor_aleatorio) en la key DESCARGAR_DATOS
+        ti.xcom_push(key='OBTENER_NUMERO', value=get_number())
+        
+        # valor de retorno para la tarea
+        return valor_aleatorio
+
+    @task
+    def tarea_duplicar(ti=None):
+        
+        # obtiene el valor que la tarea con id task_ids, "tarea_extraer" subio al xcom
+        # lee el valor usando la task_ids y la key
+        result = ti.xcom_pull(task_ids='tarea_extraer_usando_parametro_del_decorador', key='OBTENER_NUMERO')
+        
+        if result %2 ==0:
+            result = 13
+              
+        ti.xcom_push(key='DUPLICAR_NUMERO', value=get_duplicate_number(result))
+        
+        return result
+ 
+    @task
+    def tarea_size(ti=None):
+        
+        result = ti.xcom_pull(task_ids='tarea_duplicar', key='DUPLICAR_NUMERO')
+        
+        return get_len(result)
+
+    # tiene que llevar los parentesis de cada tarea sino, sale error ---> TypeError: unsupported operand type(s) for >>: '_TaskDecorator' and '_TaskDecorator'
+    tarea_numero() >> tarea_duplicar() >> tarea_size()
+    
+ejemplo_rutina_llama_codigo()
+
+########################################################################################################################################################################
+############################################################# PYTHON OPERATOR XCOM_PUSH - XCOM_PULL ####################################################################
+########################################################################################################################################################################
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+def get_number_ti(ti):
+    
+    number = get_number()
+    ti.xcom_push(key='OBTENER_NUMERO', value=number)
+    
+    return number
+    
+
+def get_duplicate_number_ti(ti):
+    
+    result = ti.xcom_pull(task_ids='mumero_operator_task_id', key='OBTENER_NUMERO')
+    
+    if result %2 ==0:
+        result = 13
+            
+    ti.xcom_push(key='DUPLICAR_NUMERO', value=get_duplicate_number(result))
+    
+    return result 
+
+def get_len_ti(ti):
+    
+        number = ti.xcom_pull(task_ids='duplicado_operator_task_id', key='DUPLICAR_NUMERO')
+        
+        ti.xcom_push(key='DUPLICAR_NUMERO', value=number)
+        
+        result =  ti.xcom_pull(task_ids='largo_operator_task_id', key='DUPLICAR_NUMERO')
+        
+        return result
+
+
+    
+with DAG(
+    dag_id="python_operator_xcom_push_xcom_pull",
+    tags=['PASA_VALORES_ENTRE_TAREAS'],
+    start_date=datetime(2021, 1, 1),
+    max_active_runs=2,
+    schedule=timedelta(minutes=30),
+    default_args={"retries": 1, "retry_delay": timedelta(minutes=5)},
+    catchup=False,
+) as dag:
+    number_operator = PythonOperator(
+        task_id="mumero_operator_task_id", python_callable=get_number_ti
+    )
+
+    duplicate_operator = PythonOperator(
+        task_id="duplicado_operator_task_id", python_callable=get_duplicate_number_ti
+    )
+
+    len_operator = PythonOperator(
+        task_id="largo_operator_task_id", python_callable=get_len_ti
+    )
+    number_operator >> duplicate_operator >> len_operator
+    
+
+########################################################################################################################################################################
+############################################################# ELIMINA CONTENIDO DE LAS KEY EN EL XCOM ##################################################################
+########################################################################################################################################################################
+import os
+from airflow.models import XCom
+from airflow.utils.db import provide_session
+
+@provide_session
+def limpiar_cache_variables_xcom(session=None):
+    
+    # captura ek nombre del dag ejecutado con la variable de AIRFLOW
+    dag_ids = os.environ["AIRFLOW_CTX_DAG_ID"]
+    session.query(XCom).filter(XCom.dag_id == dag_ids).delete()
+    
+########################################################################################################################################################################
+############################################################# TASK DECORATOR FUNCION COMO PARAMETRO ####################################################################
+########################################################################################################################################################################
+from airflow.decorators import dag, task
+
+@dag(
+    dag_id="funcion_como_parametro_elimina_xcom",
+    description='descripcion del dag creado',
+    tags=['ELIMINA_CONTENIDO_VARIABLES_XCOM'],
+    schedule_interval='@monthly', # https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html#dag-runs
+    concurrency=10,  
+    max_active_runs=3, 
+    start_date=datetime(2000, 1, 1), 
+    end_date=datetime(2029, 12, 11), 
+    catchup=False,
+    default_args={
+                "dag_owner": "https://hithub.com/jorgecardona",
+                "owner": "Jorge Cardona",  # This defines the value of the "owner" column in the DAG view of the Airflow UI
+                "retries": 2,  # If a task fails, it will retry 2 times.
+                "retry_delay": timedelta(minutes=3, ), # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
+                "do_xcom_push":False,
+                "do_xcom_pull":False
+                }
+    )
+def taskflow():
+    @task
+    def tarea_numero():
+        return get_number()
+
+    @task
+    def tarea_duplicar(number):
+        
+        if number %2 ==0:
+            return 13
+        
+        return get_duplicate_number(number)
+
+    @task
+    def tarea_size(number):
+        
+        return get_len(number)
+
+    @task
+    def eliminar_variables_xcom():
+        limpiar_cache_variables_xcom()
+            
+    # Invoke functions to create tasks and define dependencies
+    tarea_size(tarea_duplicar(tarea_numero())) >> eliminar_variables_xcom()
+
+taskflow()
+
+
+########################################################################################################################################################################
+############################################################## TASK DECORATOR XCOM_PUSH - XCOM_PULL ####################################################################
+########################################################################################################################################################################
+
+    
+@dag(
+    dag_id="resultado_como_parametro_elimina_xcom",
+    description='descripcion del dag creado',
+    tags=['ELIMINA_CONTENIDO_VARIABLES_XCOM'],
     schedule_interval='@monthly', # https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html#dag-runs
     concurrency=10,  
     max_active_runs=3, 
@@ -70,56 +312,18 @@ def ejemplo_rutina_llama_codigo():
         
         return get_len(result)
     
+    @task
+    def eliminar_variables_xcom():
+        limpiar_cache_variables_xcom()
+
     # tiene que llevar los parentesis de cada tarea sino, sale error ---> TypeError: unsupported operand type(s) for >>: '_TaskDecorator' and '_TaskDecorator'
-    tarea_numero() >> tarea_duplicar() >> tarea_size()
+    tarea_numero() >> tarea_duplicar() >> tarea_size() >> eliminar_variables_xcom()
     
 ejemplo_rutina_llama_codigo()
 
-
-@dag(
-    dag_id="obtiene_valor_de_parametro_usando_xcom_pull",
-    description='descripcion del dag creado',
-    tags=['XCOM_PUSH XCOM_PULL'],
-    schedule_interval='@monthly', # https://airflow.apache.org/docs/apache-airflow/1.10.1/scheduler.html#dag-runs
-    concurrency=10,  
-    max_active_runs=3, 
-    start_date=datetime(2000, 1, 1), 
-    end_date=datetime(2029, 12, 11), 
-    catchup=False,
-    default_args={
-                "dag_owner": "https://hithub.com/jorgecardona",
-                "owner": "Jorge Cardona",  # This defines the value of the "owner" column in the DAG view of the Airflow UI
-                "retries": 2,  # If a task fails, it will retry 2 times.
-                "retry_delay": timedelta(minutes=3, ), # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
-                "do_xcom_push":False,
-                "do_xcom_pull":False
-                }
-    )
-def taskflow():
-    
-    @task
-    def number_task():
-        return get_number()
-    
-    @task
-    def duplicate_task(number):
-        
-        if number %2 == 0:
-            return 13
-        return get_duplicate_number(number)
-    
-    @task
-    def len_task(number):
-        return get_len(number)
-
-    len_task(duplicate_task(number_task()))
-
-
-dag = taskflow()
-
-#########################################################################################################################################################################
-#########################################################################################################################################################################
-#########################################################################################################################################################################
+########################################################################################################################################################################
+############################################################# PYTHON OPERATOR XCOM_PUSH - XCOM_PULL ####################################################################
+########################################################################################################################################################################
 
 def get_number_ti(ti):
     
@@ -150,13 +354,11 @@ def get_len_ti(ti):
         
         return result
 
-#########################################################################################################################################################################
-#########################################################################################################################################################################
-#########################################################################################################################################################################
 
+    
 with DAG(
-    "xcom_dag",
-    tags=['PYTHON_OPERATOR - XCOM_PUSH XCOM_PULL'],
+    dag_id="python_operator_xcom_push_xcom_pull_elimina_xcom",
+    tags=['ELIMINA_CONTENIDO_VARIABLES_XCOM'],
     start_date=datetime(2021, 1, 1),
     max_active_runs=2,
     schedule=timedelta(minutes=30),
@@ -174,5 +376,11 @@ with DAG(
     len_operator = PythonOperator(
         task_id="largo_operator_task_id", python_callable=get_len_ti
     )
-    
-    number_operator >> duplicate_operator >> len_operator
+
+    delete_xcom = PythonOperator(
+        task_id="delete_xcom",
+        python_callable = limpiar_cache_variables_xcom,
+        dag=dag
+    )
+	  
+    number_operator >> duplicate_operator >> len_operator >> delete_xcom
