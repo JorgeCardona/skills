cbab3d9a9c3f40a05c90d7991e75a9049012fe29
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-03-05 23:28:46 -0500

    comparacion dags de scheduler con tiempo, de crontab, days_ago y deltatime

---
+++ Airflow dags/delta_time_days_ago.py
@@ -0,0 +1,79 @@
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+
+# con el parametro 'kwargs', se obtiene toda la informacion de la ejecucion del DAG
+def success_message(kwargs):
+    print(f'Task Completed Successfully -> parametros del DAG y TAREA {kwargs}')
+# con el parametro 'kwargs', se obtiene toda la informacion de la ejecucion del DAG
+def fail_message(kwargs):
+    print(f'ERROR task Failed -> {kwargs}')
+    
+@dag(
+    dag_id="delta",
+    tags=['times'],
+    schedule_interval= timedelta(minutes=5), # https://docs.python.org/es/3/library/datetime.html#timedelta-objects
+    start_date=datetime(2023, 3, 5, 20, 39, 0), 
+    end_date=datetime(2029, 12, 11),
+    catchup=False, # backfilling process NO SE EJECUTA
+    )
+def delta():
+    import random
+    valor_aleatorio = random.randint(1, 20_000)
+    
+    @task(task_id='tarea_delta_time')
+    def tarea_numero():
+        return random.randint(1, 1_000_000)
+
+    tarea_numero() 
+    
+delta()
+
+@dag(
+    dag_id="crontab",
+    tags=['times'],
+    schedule_interval='*/5 * * * *', # https://crontab.guru/#*/5_*_*_*_*
+    start_date=datetime(2023, 3, 5, 20, 39, 0), 
+    end_date=datetime(2029, 12, 11),
+    catchup=True, # backfilling process ESTA POR DEFECTO en True Y SE EJECUTA
+    max_active_runs=5 # es la maxima cantidad de DAGs corriendo cuando hay un backfilling, para no saturar la maquina y acabar con los recursos
+    )
+def cron():
+    import random
+    valor_aleatorio = random.randint(1, 20_000)
+    
+    @task(task_id='tarea_crontab')
+    def tarea_numero():
+        return random.randint(1, 1_000_000)
+
+    tarea_numero() 
+    
+cron()
+
+
+from airflow.utils.dates import days_ago
+
+@dag(
+    dag_id="days_ago",
+    tags=['times'],
+    schedule_interval='@daily', # https://docs.python.org/es/3/library/datetime.html#timedelta-objects
+    start_date=days_ago(5), 
+    end_date=datetime(2029, 12, 11),
+    #catchup=True, # backfilling process ESTA POR DEFECTO en True Y SE EJECUTA
+    )
+def days():
+    import random
+    valor_aleatorio = random.randint(1, 20_000)
+    
+    @task(task_id='tarea_days_ago',
+          retries=3, # si la tarea falla cuantas veces va a reintentar la tarea para completar la tarea exitosamente
+          retry_delay=timedelta(minutes=5), # cuanto tiempo espera entre cada reintento que la tarea falla
+          on_success_callback=success_message, # accion que hace si la tarea se completa de manera exitosa
+          on_failure_callback=fail_message, # accion que hace si la tarea FALLA
+          # retries Y retry_delay son argumento que se pueden declarar en el DAG como argumentos por defecto y afecta todas los OPERATORS o tasks del DAG
+          )
+    def tarea_numero(uno='parametro 1',dos='parametro 2'):
+        return random.randint(1, 1_000_000)
+
+    tarea_numero() 
+    
+days()
