88f2298e8825d3825cd88566c9d8a0ce44ccd727
PARADIGMA\QiDimMak <jorgecardona@utp.edu.co>
2023-04-01 13:17:07 -0500

    adicionado el JOIN merge columns

--- Pandas/utilidades.py
+++ Pandas/utilidades.py
@@ -9,12 +9,12 @@ animales = {
     'Vertebrados':['Ballenas','Capibaras','Ratones','Delfines','Ovejas','Canguros','Orcas','Elefantes','Chimpancés','Perros','Jirafas','Gorilas','Gatos','Rinocerontes','Seres humanos','Caballos','Leones','Cabras','Vacas','Hienas','Víboras','Basiliscos','Lagartos voladores australianos','Cocodrilos','Boas constrictoras','Monstruos de Gila','Aligátores','Tortugas galápagos','Luciones','Iguanas','Tortugas terrestres','Dinosaurios (extintos)','Tortugas acuáticas','Varanos','Pterosaurios (extintos)','Lagartijas comunes','Camaleones','Mosasaurios (extintos)','Dragones de Komodo','Tuátaras de Nueva Zelanda','Tiburones','Bacalaos','Peces vela','Anguilas de mar','Peces sapo','Atunes','Barracudas','Koi japoneses','Peces espada','Peces globo','Carpas','Peces abisales','Peces payaso','Esturiones','Celacantos (extintos)','Las agujas de río','Pirañas','Lenguados','Los tiburones ballena','Peces luna','Ajolotes','Sapos corredores','Ranas toro africanas','Ranas arborícolas','Sapos gigantes','Salamandras gigantes chinas','Cecilias','Ranas venenosas','Necturos','Salamandras comunes','Gallipatos','Microsaurios (extintos)','Ranas arcoíris','Salamandras sin pulmón','Lisorofios (extintos)','Ranas de Darwin','Ranas de Seychelles','Proteos','Sapos comunes','Sapos de caña venenosos','Gallinas','Pájaros carpinteros','Guacamayas','Patos','Lechuzas','Loros','Buitres','Cóndores','Águilas','Cuervos','Colibríes','Avestruces','Tucanes','Canarios','Cigüeñas','Pavos','Gaviotas','Pavorreales','Faisanes','Pelícanos']    ,
     'Invertebrados':['Moscas domésticas','Tábano bombilius','Arañas','Avispas excavadoras','Mariposa limonera','Mariposa saltacercas','Orugas','Libélula emperador','Libélulas','Escorpiones','Mariposa sesia','Escarabajo','Mariposa macaón','Mariposa blanquita de la col','Araña de patas largas','Hormigas león','Polilla','Luciérnaga','Avispas alfareras','Escarabajo fitófago','Araña cangrejo','Chinche roja, zapatero','Curilla o aceitera','Cigarra o chicharra','Mariquitas','Caballito del diablo','Avispa scolia','Mariposa cervantes','Termes','Ácaros','Abeja de la miel','Mosquitos','Moscardones','Caracoles','Cochinillas','Tijeretas','Garrapata','Mariposa dorada','Piojos','Cucarachas','Mariposa pavo real','Mariposa del almez','Pulgas','Abejorros','Mantis religiosa','Medusas','Grillos','Moscas cernícalo','Mariposa esfinge colibrí','Avispas portasierra','Mariposa nacarada','Estrellas de mar','Hormigas','Chinches','Tábanos','Escarabajos acuáticos','Milpiés','Larvas','Cochinillas de humedad','Pulgones','Escarabajos','Mariposa medioluto','Avispas sociales','Mariposa ajedrezada menor','Mariposa ícaro','Abejas','Saltamontes','Mariposa lobito agreste','Ciempiés'],
     'Ovíparos':['Cocodrilo','Hormiga','Pingüino','Gorrión','Gallina','Loro','Cóndor','Águila','Avestruz','Cigüeña','Pato','Ganso','Víbora de cascabel','Codorniz','Rana','Guacamayo','Hornero','Salmón','Abeja','Paloma'],
-    'Frugívoros':['Ardilla','Mono aullador','Loro','Bonobo','Mosca de la fruta','Macaco','Calandria','Murciélago frugívoro','Iguana','Chimpancé','Tupaya enana','Conejo del campo','Chinche del campo','Perico','Tucán','Gibón','Pacú','Zarigüeya','Gorila','Tortuga terrestre','Zorro volador','Lémur','Tapir','Orangután','Lirón','Mono Tití'],   
-    'Rumiantes':['Vaca','Toro','Oveja','Alce','Cabra','Llama','Cebú','Búfalo cafre','Ciervo común','Nilgó','Antílope de cuatro cuernos','Gamo común','Sitatunga','Niala','Ciervo de copete','Anoa de montaña','Bongo','Alce irlandés','Ñu','Venado temazate','Sambar','Gayal','Bisonte estepario','Ciervo porcino','Ciervo de los pantanos','Búfalo de agua','Gacela de Grant','Corzo','Antílope jeroglífico','Candelillo','Venado andino','Bisonte americano','Corzuela','Vicuña','Bisonte europeo','Oryx','Urial','Venado de las Pampas','Uapití','Anoa de llanura','Reno','Apaca','Íbice de los Alpes','Ciervo axis','Impala'],    
+    'Frugívoros':['Ardillas','Mono aullador','Loros','Bonobos','Mosca de la fruta','Macacos','Calandrias','Murciélago frugívoro','Iguana','Chimpancés','Tupaya enana','Conejo del campo','Chinche del campo','Pericos','Tucán','Gibón','Pacú','Zarigüeya','Gorilas','Tortuga terrestre','Zorro volador','Lémur','Tapir','Orangután','Lirón','Mono Tití'],   
+    'Rumiantes':['Vacas','Toros','Ovejas','Alces','Cabras','Llamas','Cebú','Búfalo cafre','Ciervo común','Nilgó','Antílope de cuatro cuernos','Gamo común','Sitatunga','Niala','Ciervo de copete','Anoa de montaña','Bongo','Alce irlandés','Ñu','Venado temazate','Sambar','Gayal','Bisonte estepario','Ciervo porcino','Ciervo de los pantanos','Búfalo de agua','Gacela de Grant','Corzos','Antílope jeroglífico','Candelillo','Venado andino','Bisonte americano','Corzuela','Vicuña','Bisonte europeo','Oryx','Urial','Venado de las Pampas','Uapití','Anoa de llanura','Renos','Apacas','Íbice de los Alpes','Ciervo axis','Impala'],    
     
-    'Herbívoros':['Cebra','Oveja','Antílope','Llama','Cabra','Alpaca','Jirafa','Koala','Elefante','Ciervo','Tapir','Ardilla','Vaca','Canguro','Caballo','Cobayo','Rinoceronte','Burro','Conejo','Tortuga'],
-    'Carnívoros':['Foca','Hiena','Lince','Gato','Jaguar','Lobo','Gato montés','León','Lobo gris','Comadreja','León marino','Civeta','Coyote','Leopardo','Mangosta','Marta','Cachalote','Tigre siberiano','Ballena azul','Delfín','Tigre de bengala','Ballena jorobada','Oso pardo','Orca','Beluga','Oso polar','Nutria','Narval','Guepardo','Gineta manchada','Perro','Puma','Panda rojo','Pantera negra','Gineta común','Linsangs','Fosa','Murciélago espectral','Mapache','Visón europeo','Murciélago pescador','Demonio de Tasmania','Serval','Morsa','Chacal','Pangolín','Hurón','Glotón','Tejón','Garduña','Kinkajú','Anaconda','Cobra','Tortuga marina','Boa','Pitón','Varano del desierto','Cocodrilo','Tortuga lagarto','Caimán','Dragón de Komodo','Gecko leopardo','Serpiente de coral','Atún','Pez espada','Muskallonga americano','Tiburón blanco','Perca','Marlín','Tiburón martillo','Salmón','Pez gato','Tiburón tigre','Tollo cigarro','Piraña','Tiburón peregrino','Tiburón toro','Barracuda','Aguila arpía','Albatros','Buitre leonado','Águila pescadora','Gaviota','','Buitre quebrantahuesos','Secretario','Halcón','Buitre común','Pingüino','Cuervo','Buitre negro','Pelícano','Cóndor de California','Marabú','Milano','Cóndor andino','Lechuza','Alimoche','Búho','Gavilán pollero'],
-    'Omnívoros':['Humano','Seres humanos','Erizo','Mirlo','Perdiz','Flamingo','Bacalao','Gaviota','Garcilla bueyera','Focha','Casuario','Zorzal','Pájaro carpintero','Zorrillo','Perro','Delfín','Grajo','Pez globo','Pinzón','Labeo bicolor','Hormiga','Garza real','Petirrojo','Morena','Jabalí','Cerdo','Tucán','Gorrión','Mono','Urraca','Gallina','Corydora','Zarigüeya','Cacatúa','Cangrejo','Avispa','Pez espiga','Tiburón','Rinoceronte','Langosta','Ballena','Avestruz','Tortugas','Faisán','Cisne','Gatos','Mosca','Bengalí rojo','Oso','Hámster','Cuervos','Ñandú','Bagre','Avutarda','Lémur','Zorro','Armadillo','Pez arquero','Mofeta','Mapache','Gorila','Chimpancé','Ardilla','Ornitorrinco','Emú','Grillo','Avestruces','Ratón','Carpa','Cucaracha','Escarabajo de cementerio','Pavo real','Ganso','Coyote','Piraña','Coatí','Manatí','Grulla','Mojarrita','Ratones','Nutria','Jerbo','Casuarios','Tejón','Tortuga','Tortuga carbonaria','Espátula','Perezoso','Ayeaye','Calamón'],
+    'Herbívoros':['Cebras','Ovejas','Antílopes','Llamas','Cabras','Alpacas','Jirafas','Koalas','Elefante','Ciervos','Tapires','Ardillas','Vacas','Canguro','Caballos','Cobayos','Rinocerontes','Burros','Conejos','Tortugas'],
+    'Carnívoros':['Focas','Hienas','Linces','Gatos','Jaguar','Lobos','Gato montés','León','Lobo gris','Comadrejas','León marino','Civetas','Coyotes','Leopardos','Mangostas','Martas','Cachalotes','Tigre siberiano','Ballena azul','Delfín','Tigre de bengala','Ballena jorobada','Oso pardo','Orcas','Belugas','Oso polar','Nutrias','Narval','Guepardos','Gineta manchada','Perros','Pumas','Panda rojo','Pantera negra','Gineta común','Linsangs','Fosas','Murciélago espectral','Mapaches','Visón europeo','Murciélago pescador','Demonio de Tasmania','Serval','Morsa','Chacal','Pangolín','Hurón','Glotón','Tejón','Garduña','Kinkajú','Anacondas','Cobras','Tortuga marina','Boas','Pitón','Varano del desierto','Cocodrilos','Tortuga lagarto','Caimán','Dragón de Komodo','Gecko leopardo','Serpiente de coral','Atún','Pez espada','Muskallonga americano','Tiburón blanco','Percas','Marlín','Tiburón martillo','Salmón','Pez gato','Tiburón tigre','Tollo cigarro','Pirañas','Tiburón peregrino','Tiburón toro','Barracuda','Aguila arpía','Albatros','Buitre leonado','Águila pescadora','Gaviota','','Buitre quebrantahuesos','Secretario','Halcón','Buitre común','Pingüino','Cuervo','Buitre negro','Pelícano','Cóndor de California','Marabú','Milano','Cóndor andino','Lechuza','Alimoche','Búho','Gavilán pollero'],
+    'Omnívoros':['Humano','Seres humanos','Erizos','Mirlos','Perdiz','Flamingso','Bacalaos','Gaviotas','Garcilla bueyera','Fochas','Casuarios','Zorzal','Pájaro carpintero','Zorrillos','Perros','Delfines','Grajos','Pez globo','Pinzón','Labeo bicolor','Hormigas','Garza real','Petirrojos','Morenas','Jabalies','Cerdos','Tucán','Gorrión','Monos','Urracas','Gallinas','Corydora','Zarigüeyas','Cacatúa','Cangrejos','Avispas','Pez espiga','Tiburón','Rinocerontes','Langostas','Ballena','Avestruz','Tortugas','Faisán','Cisnes','Gatos','Moscas','Bengalí rojo','Osos','Hámster','Cuervos','Ñandú','Bagres','Avutarda','Lémur','Zorros','Armadillos','Pez arquero','Mofetas','Mapache','Gorilas','Chimpancé','Ardilla','Ornitorrinco','Emú','Grillos','Avestruces','Ratón','Carpa','Cucaracha','Escarabajo de cementerio','Pavo real','Gansos','Coyotes','Pirañas','Coatí','Manatí','Grullas','Mojarrita','Ratones','Nutria','Jerbo','Casuarios','Tejón','Tortugas','Tortuga carbonaria','Espátula','Perezosos','Ayeaye','Calamón'],
 }
 
 
@@ -83,28 +83,57 @@ def crear_dataframes(limite_muestra = 100):
     # retorna los diccionarios como dataframes usando la funcion creada para esa conversion
     return pd.DataFrame(diccionario_0), pd.DataFrame(diccionario_1), pd.DataFrame(diccionario_2)
 
-def join_dataframe(primer_dataframe, segundo_dataframe, llaves_para_join = '', tipo_de_join='INNER', mostrar_tipo_join=False, self_join=['','']):
+def join_dataframe(primer_dataframe, segundo_dataframe, llaves_para_join = [''], tipo_de_join='INNER', mostrar_tipo_join=False, self_join=['',''], sort=False):
     
     import pandas as pd
-
-    # une los dataframe con el JOIN y elimina registros duplicados con .drop_duplicates()
-    if tipo_de_join.lower() == 'cross':
-        dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how=tipo_de_join.lower())
-        tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}')"  
-        
-    elif tipo_de_join.lower() == 'self':
-        dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on=self_join[0], right_on=self_join[1], how='left', suffixes=['_left','_right'], indicator=mostrar_tipo_join)
-        tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on={self_join[0]}, right_on={self_join[1]}, how='left', suffixes=['_left','_right'], indicator={mostrar_tipo_join})" 
-        
-    elif tipo_de_join.lower() == 'index':
-        dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_index=True, right_index=True)
-        tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}')"
-    else:        
-        dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), on=llaves_para_join, how=tipo_de_join.lower(), indicator=mostrar_tipo_join)
-        dataframe_unido = dataframe_unido.sort_values(by=llaves_para_join, ascending=False)
-        dataframe_unido = dataframe_unido.reset_index()
-        tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), on={llaves_para_join}, how='{tipo_de_join.lower()}', indicator={mostrar_tipo_join})" 
-       
-    print(tipo_join)
     
-    return dataframe_unido
+    tipo_de_join = tipo_de_join.lower()
+    joins_disponibles = ['inner','left','right','outer','cross','self','index','columns']
+    
+    print(f'joins_disponibles = {joins_disponibles}')
+    print(f"merge parametros-> pandas.merge(df_left, df_right, how={joins_disponibles}, on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True, indicator=False, validate=None)")
+    print()
+    
+    if tipo_de_join in joins_disponibles:
+        
+        if tipo_de_join == 'inner':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), on=llaves_para_join, how=tipo_de_join.lower(), sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), on={llaves_para_join}, how='{tipo_de_join.lower()}', sort={sort}, indicator={mostrar_tipo_join})"
+
+        # une los dataframe con el JOIN y elimina registros duplicados con .drop_duplicates()
+        if tipo_de_join == 'left':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how=tipo_de_join.lower(), sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}, sort={sort}, indicator={mostrar_tipo_join}')"  
+
+        # une los dataframe con el JOIN y elimina registros duplicados con .drop_duplicates()
+        if tipo_de_join == 'right':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how=tipo_de_join.lower(), sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}, indicator={mostrar_tipo_join}')"  
+
+        # une los dataframe con el JOIN y elimina registros duplicados con .drop_duplicates()
+        if tipo_de_join == 'outer':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how=tipo_de_join.lower(), sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(),  on={llaves_para_join}, how='{tipo_de_join.lower()}, sort={sort}, indicator={mostrar_tipo_join}')"  
+
+        # une los dataframe con el JOIN y elimina registros duplicados con .drop_duplicates()
+        if tipo_de_join == 'cross':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how=tipo_de_join.lower(), sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}, sort={sort}, indicator={mostrar_tipo_join}')"  
+
+        elif tipo_de_join == 'self':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on=self_join[0], right_on=self_join[1], how='left', suffixes=['_left','_right'], sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on={self_join[0]}, right_on={self_join[1]}, how='left', suffixes=['_left','_right'], sort={sort}, indicator={mostrar_tipo_join})" 
+
+        elif tipo_de_join == 'index':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_index=True, right_index=True, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), how='{tipo_de_join.lower()}')"
+
+        elif tipo_de_join == 'columns':
+            dataframe_unido = pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on=llaves_para_join[0], right_on=llaves_para_join[1], sort=sort, indicator=mostrar_tipo_join)
+            tipo_join = f"pd.merge(primer_dataframe.drop_duplicates(), segundo_dataframe.drop_duplicates(), left_on={llaves_para_join[0]}, right_on={llaves_para_join[1]}, sort={sort}, indicator={mostrar_tipo_join})"
+        print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print(tipo_join)
+        print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        return dataframe_unido
+    
+    return f'el tipo de join {tipo_de_join} no esta disponible para usar'
