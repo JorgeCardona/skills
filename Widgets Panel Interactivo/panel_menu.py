14cc3669e21c669bdb9b2637141ae27f39a61cd7
jorgecardona <jorgecardona@utp.edu.co>
2023-06-20 22:52:58 -0500

    adicionado acceso a menu de aplicacion

--- Widgets Panel Interactivo/panel_menu.py
+++ Widgets Panel Interactivo/panel_menu.py
@@ -60,7 +60,55 @@ def markdown_app():
 def json_app():
     return pn.pane.JSON({'abc': 123})
 
+def get_menu():
+    import panel as pn
+    import hvplot.pandas
 
+    # Load Data
+    from bokeh.sampledata.autompg import autompg_clean as df
+
+    # Make DataFrame Pipeline Interactive
+    idf = df.interactive()
+
+    # Define Panel widgets
+    cylinders = pn.widgets.IntSlider(name='Cylinders', start=4, end=8, step=2)
+    mfr = pn.widgets.ToggleGroup(
+        name='MFR',
+        options=['ford', 'chevrolet', 'honda', 'toyota', 'audi'], 
+        value=['ford', 'chevrolet', 'honda', 'toyota', 'audi'],
+        button_type='success')
+    yaxis = pn.widgets.RadioButtonGroup(
+        name='Y axis', 
+        options=['hp', 'weight'],
+        button_type='success'
+    )
+
+    # Combine pipeline and widgets
+    ipipeline = (
+        idf[
+            (idf.cyl == cylinders) & 
+            (idf.mfr.isin(mfr))
+        ]
+        .groupby(['origin', 'mpg'])[yaxis].mean()
+        .to_frame()
+        .reset_index()
+        .sort_values(by='mpg')  
+        .reset_index(drop=True)
+    )
+
+    # Pipe to hvplot
+    ihvplot = ipipeline.hvplot(x='mpg', y=yaxis, by='origin', color=["#ff6f69", "#ffcc5c", "#88d8b0"], line_width=6, height=400)
+
+    # Layout using Template
+    template = pn.template.FastListTemplate(
+        title='Interactive DataFrame Dashboards with hvplot .interactive', 
+        sidebar=[cylinders, 'Manufacturers', mfr, 'Y axis' , yaxis],
+        main=[ihvplot.panel()],
+        accent_base_color="#88d8b0",
+        header_background="#88d8b0",
+    )
+    
+    return template
 
 # Variables de Configuracion
 UBICACION_DE_LA_APLICACION = 'panel_menu:app'
@@ -78,9 +126,14 @@ app = pn.serve(
                 'Slider_Auto_Completado': autocomplete_event_select_app, # no se puede usar espacios en blanco, se debe usar el guion bajo, under score '_'
                 'Select_Event_Text': slider_select_app,
                 'markdown': markdown_app,
-                'json': json_app
+                'json': json_app,
+                'menu':get_menu
                 },
-        title={'Slider_Auto_Completado': 'A Markdown App', 'Select_Event_Text': 'A JSON App', 'markdown': 'A Markdown App', 'json': 'A JSON App'},
+        title={'Slider_Auto_Completado': 'A Markdown App',
+               'Select_Event_Text': 'A JSON App',
+               'markdown': 'A Markdown App',
+               'json': 'A JSON App',
+               'menu':'Aplication Menu'},
         port=PUERTO_EXPONER_PANELES,
         allow_websocket_origin=[# similar al cross origin
                                 URL_APLICACION_QUE_INTERACCIONA_CON_PANELES, # donde se van a desplegar los paneles
